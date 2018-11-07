068ee7e5a747e4d5d51afcfb396ddad34df9576a
dowglasbarros <dowglas_rodrigo@hotmail.com>
2018-11-07 03:50:46 +0000

    Configuring routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'user/:id/edit',
+    component: UserEditComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
