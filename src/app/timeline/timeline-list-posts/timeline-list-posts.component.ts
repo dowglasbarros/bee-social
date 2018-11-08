60606b73e594706436b348132d83aa2ed7f35aca
Avanade <Avanade@WIN-9P03YYVGPH.corp.avanade.org>
2018-11-08 10:24:00 -0200

    Functional like button

--- src/app/timeline/timeline-list-posts/timeline-list-posts.component.ts
+++ src/app/timeline/timeline-list-posts/timeline-list-posts.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 const POSTS = [
   {
-    "id": 1,
+    "id": "POST-1",
     "avatarPicture": "assets/images/avatar2.png",
     "avatarName": "John Doe",
     "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
@@ -19,7 +19,7 @@ const POSTS = [
     "likes": 3
   },
   {
-    "id": 2,
+    "id": "POST-2",
     "avatarPicture": "assets/images/avatar5.png",
     "avatarName": "Jane Doe",
     "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
@@ -27,7 +27,7 @@ const POSTS = [
     "likes": 2
   },
   {
-    "id": 3,
+    "id": "POST-3",
     "avatarPicture": "assets/images/avatar6.png",
     "avatarName": "Angie Jane",
     "description": "Have you seen this?",
@@ -55,4 +55,16 @@ export class TimelineListPostsComponent implements OnInit {
   ngOnInit() {
   }
 
+  getLike(post, postUser){
+    const btnLike = postUser.querySelector('.btn-like')
+    const classActivatedLike = 'like-activated';
+    if(btnLike.classList.contains(classActivatedLike)){
+      post.likes--;
+      btnLike.classList.remove(classActivatedLike);
+    }
+    else{
+      post.likes++;
+      btnLike.classList.add(classActivatedLike);
+    }
+  }
 }
