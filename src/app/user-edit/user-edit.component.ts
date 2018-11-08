89b7ad60278123313076b0df5a3fd64e8665aa31
dowglasbarros <dowglas_rodrigo@hotmail.com>
2018-11-08 19:31:21 +0000

    Creating user-edit component and setting ngModel

--- src/app/user-edit/user-edit.component.ts
+++ src/app/user-edit/user-edit.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-edit.component.scss']
 })
 export class UserEditComponent implements OnInit {
+  user = {
+    name: 'Dowglas Barros',
+    email: 'colaborador@avanade.com',
+    photo: "assets/images/avatar3.png",
+    position: 'Programador Sênior',
+    city: 'São Paulo',
+    company: 'São Paulo',
+    project: 'Natura',
+    groups: ['Angular', 'Node.js'],
+    first_access: true
+  }
 
   constructor() { }
 
