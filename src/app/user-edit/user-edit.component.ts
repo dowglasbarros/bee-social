7621e0e2a2da26adc5e09d922efe0f590d0bfbe4
Natalia Maimoni <nat_maimoni@hotmail.com>
2018-11-12 11:11:08 -0200

    ajuste video

--- src/app/user-edit/user-edit.component.ts
+++ src/app/user-edit/user-edit.component.ts
@@ -24,7 +24,7 @@ export class UserEditComponent implements OnInit {
   userPhoto = 'assets/images/avatar3.png';
 
   /* tratando o array de grupos */
-  groupArray = this.user.groups.filter(function(group) {
+  groupArray = this.user.groups.filter(function (group) {
     return group.follow;
   });
   groupLabels = this.groupArray.map(group => group.groupLabel).join(', ');
@@ -49,12 +49,12 @@ export class UserEditComponent implements OnInit {
 
   onSubmit() {
     this.userService.updateUser(this.user)
-        .subscribe(valor => {
-          this.router.navigateByUrl('/user/' + this.user.id);
-          alert('Usuário atualizado com sucesso');
-          }, error => {
-            alert('Erro ao atualizar, entre em contato com o suporte.');
-        });
+      .subscribe(valor => {
+        this.router.navigateByUrl('/user/' + this.user.id);
+        alert('Usuário atualizado com sucesso');
+      }, error => {
+        alert('Erro ao atualizar, entre em contato com o suporte.');
+      });
   }
 
 }
