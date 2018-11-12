48000ad75c5b383e54ab61d3fa40566efff41b30
Avanade <Avanade@WIN-9P03YYVGPH.corp.avanade.org>
2018-11-12 08:55:09 -0200

    create post test

--- src/app/timeline/timeline-create-post/timeline-create-post.component.spec.ts
+++ src/app/timeline/timeline-create-post/timeline-create-post.component.spec.ts
@@ -3,6 +3,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { TimelineCreatePostComponent } from './timeline-create-post.component';
+import { TimelineListPostsComponent } from '../timeline-list-posts/timeline-list-posts.component';
 
 describe('TimelineCreatePostComponent', () => {
   let component: TimelineCreatePostComponent;
@@ -11,7 +12,7 @@ describe('TimelineCreatePostComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, RouterTestingModule],
-      declarations: [TimelineCreatePostComponent]
+      declarations: [TimelineCreatePostComponent, TimelineListPostsComponent]
     })
       .compileComponents();
   }));
@@ -24,10 +25,13 @@ describe('TimelineCreatePostComponent', () => {
 
   it('should create a post', () => {
     const compiled = fixture.debugElement.nativeElement;
+    let fixtureTimelineListPosts = TestBed.createComponent(TimelineListPostsComponent);
+    const compiledTimelineListPosts = fixtureTimelineListPosts.debugElement.nativeElement;
     let textPost = "Lorem ipsum";
     compiled.querySelector('#input-post').value = textPost;
     component.insertPost();
-    expect(compiled.querySelector('#post-user-0 .list-posts-text p').textContent).toEqual(textPost);
+    let textFirstPost = compiledTimelineListPosts.querySelector('#post-user-0 .list-posts-text p').textContent;
+    expect(textFirstPost).toEqual(textPost);
   });
 
 });
